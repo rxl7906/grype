81d48848f26ec023e35479263e7db826dccc2214
Robin Li <lrobin@vmware.com>
2021-06-25 15:45:37 -0400

    Use lowercase 'l' for license because that's what is in cyclonedx grype output. Remove v: to look at all namespaces.

--- grype/presenter/cyclonedx/component.go
+++ grype/presenter/cyclonedx/component.go
@@ -13,11 +13,11 @@ type Component struct {
 	Name        string     `xml:"name"`                  // Required; The name of the Component as defined by the project
 	Version     string     `xml:"version"`               // Required; The version of the Component as defined by the project
 	Description string     `xml:"description,omitempty"` // A description of the Component
-	Licenses    *[]License `xml:"licenses>License"`      // A node describing zero or more License names, SPDX License IDs or expressions
+	Licenses    *[]License `xml:"licenses>license"`      // A node describing zero or more License names, SPDX License IDs or expressions
 	PackageURL  string     `xml:"purl,omitempty"`        // Specifies the package-url (PackageURL). The purl, if specified, must be valid and conform to the specification defined at: https://github.com/package-url/purl-spec
 	// TODO: source, hashes, copyright, cpe, purl, swid, modified, pedigree, externalReferences
 	// TODO: add user-defined parameters for syft-specific values (image layer index, cataloger, location path, etc.)
-	Vulnerabilities *[]Vulnerability `xml:"v:vulnerabilities>v:vulnerability,omitempty"`
+	Vulnerabilities *[]Vulnerability `xml:"vulnerabilities>vulnerability,omitempty"`
 }
 
 // License represents a single software License for a Component
